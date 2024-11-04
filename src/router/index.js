686fd7be8be70f94961a0d688512eebcf627d604
patrickphandera <patrickphandera@gmail.com>
2024-11-04 13:42:11 +0200

    hshhshshs

--- src/router/index.js
+++ src/router/index.js
@@ -58,21 +58,9 @@ const router = createRouter({
   routes,
 });
 
-// Navigation Guard
-// router.beforeEach((to, from, next) => {
-//   // List of routes that don’t require authentication
-//   const publicPages = ['HomePage', 'AboutPage', 'Register', 'Login', 'Otp'];
-//   const authRequired = !publicPages.includes(to.name);
-//   const isAuthenticated = localStorage.getItem('token'); // Replace 'token' with the key name used in localStorage
 
-//   if (authRequired && !isAuthenticated) {
-//     next('/login'); // Redirect to login page if not authenticated
-//   } else {
-//     next(); // Continue to the route if authenticated or accessing a public page
-//   }
-// });
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = Boolean(localStorage.getItem('token')); // Adjust if token is stored differently
+  const isAuthenticated = Boolean(localStorage.getItem('token')); 
   if (to.name !== 'Login' && !isAuthenticated) {
     next({ name: 'Login' });
   } else {
